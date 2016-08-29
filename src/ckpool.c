1fa201bfedab5335da5d7d910e6a8f17cf6aa71a
Martín Medina <tinnmedina@gmail.com>
2016-08-29 20:06:16 -0300

    RSK-1249. Disable RSK log for GBT request and response

--- src/ckpool.c
+++ src/ckpool.c
@@ -865,7 +865,9 @@ json_t *json_rpc_call_timeout(connsock_t *cs, const char *rpc_req, float timeout
 		 "Content-Length: %d\n\n%s",
 		 cs->auth, cs->url, cs->port, len, rpc_req);
 
-	LOGINFO_RSK("ROOTSTOCK: json_rpc_call: %p, %s", http_req, rpc_req);
+	if (strstr(http_req, "getblocktemplate") == NULL) {
+		LOGINFO_RSK("ROOTSTOCK: json_rpc_call: %p, %s", http_req, rpc_req);
+	}
 
 	len = strlen(http_req);
 	tv_time(&stt_tv);
@@ -909,7 +911,9 @@ json_t *json_rpc_call_timeout(connsock_t *cs, const char *rpc_req, float timeout
 			   elapsed, __func__, rpc_method(rpc_req));
 	}
 
-	LOGINFO_RSK("ROOTSTOCK: json_rpc_reply: %p, %s", http_req, cs->buf);
+	if (strstr(http_req, "getblocktemplate") == NULL) {
+		LOGINFO_RSK("ROOTSTOCK: json_rpc_reply: %p, %s", http_req, cs->buf);
+	}
 
 	val = json_loads(cs->buf, 0, &err_val);
 	if (!val) {
