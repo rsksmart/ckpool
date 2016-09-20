c4b4ffbf88a07155d7a822944b21fea33652945b
aeidelman <adrian@rootstock.io>
2016-09-20 15:50:24 -0300

    Test

--- src/stratifier.c
+++ src/stratifier.c
@@ -697,9 +697,9 @@ static void generate_coinbase(const ckpool_t *ckp, workbase_t *wb)
 /*
 	if (ckp->rskds) {
 		wb->coinb2len += 8;
-		wb->coinb2bin[wb->coinb2len++] = 9 + 32;
-		memcpy(wb->coinb2bin + wb->coinb2len, "\x52\x53\x4B\x42\x4C\x4F\x43\x4B\x3A", 9);
-		wb->coinb2len += 9;
+		wb->coinb2bin[wb->coinb2len++] = 10 + 32;
+		memcpy(wb->coinb2bin + wb->coinb2len, "\x6A\x52\x53\x4B\x42\x4C\x4F\x43\x4B\x3A", 10);
+		wb->coinb2len += 10;
 		memcpy(wb->coinb2bin + wb->coinb2len, wb->rsk_blockheaderbin, 32);
 		wb->coinb2len += 32;
 	}
